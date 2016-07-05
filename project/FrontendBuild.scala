d2be7b39ccbb3078f6e6e10891cb4da46bd09cdc
Andrew Walters <andrew.walters@digital.hmrc.gov.uk>
2016-07-05 13:00:32 +0100

    Importing clamav-client library (2.0.0)

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -44,6 +44,7 @@ private object AppDependencies {
   private val hmrcTestVersion = "1.6.0"
   private val playReactivemongoVersion = "4.8.0"
   private val simpleReactivemongoVersion = "4.8.0"
+  private val clamavClientVersion = "2.0.0"
 
   val compile = Seq(
     ws,
@@ -56,7 +57,8 @@ private object AppDependencies {
     "uk.gov.hmrc" %% "play-json-logger" % playJsonLoggerVersion,
     "uk.gov.hmrc" %% "govuk-template" % govukTemplateVersion,
     "uk.gov.hmrc" %% "play-health" % playHealthVersion,
-    "uk.gov.hmrc" %% "play-ui" % playUiVersion
+    "uk.gov.hmrc" %% "play-ui" % playUiVersion,
+    "uk.gov.hmrc" %% "clamav-client" % clamavClientVersion
   )
 
   trait TestDependencies {
