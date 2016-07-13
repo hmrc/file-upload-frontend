ba3b5f9458822cb428dae4b54cfbabb8a39baa97
Andrew Walters <andrew.walters@digital.hmrc.gov.uk>
2016-07-14 00:18:29 +0100

    Work on persistence down to backend service. Streaming put initial work (cherry picked from commit 1b60e53)

--- app/uk/gov/hmrc/fileupload/connectors/FileUploadConnector.scala
+++ app/uk/gov/hmrc/fileupload/connectors/FileUploadConnector.scala
@@ -16,6 +16,12 @@
 
 package uk.gov.hmrc.fileupload.connectors
 
+import java.nio.ByteBuffer
+
+import com.ning.http.client.{AsyncHttpClient, RequestBuilder}
+import com.ning.http.client.providers.netty.FeedableBodyGenerator
+import play.api.libs.iteratee.Iteratee
+import play.api.libs.ws.WS
 import uk.gov.hmrc.fileupload.Errors.EnvelopeValidationError
 import uk.gov.hmrc.fileupload.WSHttp
 import uk.gov.hmrc.play.config.ServicesConfig
@@ -32,10 +38,45 @@ trait FileUploadConnector {
   val http: HttpGet = WSHttp
   val baseUrl: String = baseUrl("file-upload")
 
-  def okStatusToSuccess(envelopeId: String): PartialFunction[HttpResponse, Try[String]] = { case r if r.status == 200 => Success(envelopeId) }
-  def envelopeFailure(envelopeId: String): PartialFunction[Throwable, Try[String]] = { case _ => Failure(EnvelopeValidationError(envelopeId)) }
+  def okStatusToSuccess[A](envelopeId: A): PartialFunction[HttpResponse, Try[A]] = { case r if r.status == 200 => Success(envelopeId) }
+  def envelopeFailure[A](envelopeId: String): PartialFunction[Throwable, Try[A]] = { case _ => Failure[A](EnvelopeValidationError(envelopeId)) }
 
   def validate(envelopeId: String)(implicit hc: HeaderCarrier): Future[Try[String]] = {
     http.GET(s"$baseUrl/file-upload/envelope/$envelopeId") map okStatusToSuccess(envelopeId) recover envelopeFailure(envelopeId)
+  }
+
+
+  // SEE: http://zhpooer.github.io/2014/08/20/play-for-scala-web-services-&-iteratees-&-websockets/
+  def putFile(fileData: FileData)(implicit hc: HeaderCarrier): Future[Try[Boolean]] = {
+    import play.api.Play.current
+
+    val client: AsyncHttpClient = WS.client.underlying
+
+    val bodyGenerator = new FeedableBodyGenerator()
+
+    val request = new RequestBuilder("PUT")
+      .setUrl(s"$baseUrl/file-upload/envelope/${fileData.envelopeId}/file/${fileData.fileId}/content")
+      .setBody(bodyGenerator)
+      .build()
+
+    val responseFuture = client.executeRequest(request)
+
+    val it = Iteratee.fold[Array[Byte], FeedableBodyGenerator](bodyGenerator) { (generator, bytes) =>
+      val isLast = false
+      generator.feed(ByteBuffer.wrap(bytes), isLast)
+      generator
+    } map { generator =>
+      val isLast = true
+      generator.feed(ByteBuffer.wrap(Array[Byte]()), isLast)
+
+      val res = responseFuture.get()
+
+      res.getStatusCode match {
+        case 200 => Success(true)
+        case _ => Failure(EnvelopeValidationError(fileData.envelopeId))
+      }
+    }
+
+    fileData.data |>>> it
   }
 }
--- test/uk/gov/hmrc/fileupload/connectors/FileUploadConnectorSpec.scala
+++ test/uk/gov/hmrc/fileupload/connectors/FileUploadConnectorSpec.scala
@@ -24,6 +24,7 @@ import uk.gov.hmrc.play.config.ServicesConfig
 import uk.gov.hmrc.play.http.{HeaderCarrier, HttpGet}
 import uk.gov.hmrc.fileupload.UploadFixtures._
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 class FileUploadConnectorSpec extends WireMockSpec {
@@ -32,15 +33,15 @@ class FileUploadConnectorSpec extends WireMockSpec {
   implicit val hc = HeaderCarrier()
 
   "The fileUploadConnector" should {
-    "result in a ValidEnvelope response for a valid envelopeId" in new TestFileUploadConnector(wiremockBaseUrl) {
+    "result in a ValidEnvelope response for a valid envelopeId" in new TestFileUploadConnector {
       await(validate(validEnvelopeId)) should be (Success(validEnvelopeId))
     }
 
-    "result in an InvalidEnvelope response for an invalid envelopeId" in new TestFileUploadConnector(wiremockBaseUrl) {
+    "result in an InvalidEnvelope response for an invalid envelopeId" in new TestFileUploadConnector {
       await(validate("invalidId")) should be (Failure(EnvelopeValidationError("invalidId")))
     }
 
-    "result in an InvalidEnvelope response for a sealed envelopeId" in new TestFileUploadConnector(wiremockBaseUrl) {
+    "result in an InvalidEnvelope response for a sealed envelopeId" in new TestFileUploadConnector {
       await(validate("2f816e24-1316-408d-aa2c-ba188c2090d9")) should be (Failure(EnvelopeValidationError("2f816e24-1316-408d-aa2c-ba188c2090d9")))
     }
 
@@ -48,10 +49,25 @@ class FileUploadConnectorSpec extends WireMockSpec {
       val connector = new FileUploadConnector with ServicesConfig {}
       connector.baseUrl should be ("http://localhost:8898")
     }
+
+    "result in a Success[Boolean] for a valid file upload" in new TestFileUploadConnector {
+      pending
+
+      val fileData = FileData(data = testFile, name = "TEST.out", contentType = "text/plain", envelopeId = "fea8cc15-f2d1-4eb5-b10e-b892bcbe94f8", fileId = "1")
+
+      await(putFile(fileData)) should be (Success(true))
+    }
+
+    "result in a Failure[Boolean] for an invalid file upload" in new TestFileUploadConnector {
+      pending
+
+      val fileData = FileData(data = testFile, name = "TEST.out", contentType = "text/plain", envelopeId = "invalidId", fileId = "1")
+
+      await(putFile(fileData)) should be (Failure(EnvelopeValidationError("invalidId")))
+    }
   }
 
-  class TestFileUploadConnector(url:String) extends FileUploadConnector with ServicesConfig {
-    override val baseUrl: String = url
-    override val http: HttpGet = WSHttp
+  trait TestFileUploadConnector extends FileUploadConnector with ServicesConfig {
+    override val baseUrl = wiremockBaseUrl
   }
 }
