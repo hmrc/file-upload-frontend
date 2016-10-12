e38e34f51d4f73e3861043d342853973b55aacf9
ralphcollett <ralph.collett@gmail.com>
2016-10-12 11:19:21 +0100

    FILE-241 Stubbing out Clam for testing.

---
+++ it/uk/gov/hmrc/fileupload/FakeClam.scala
@@ -0,0 +1,43 @@
+package uk.gov.hmrc.fileupload
+
+import java.io.{BufferedReader, DataOutputStream, InputStreamReader}
+import java.net.ServerSocket
+
+import play.api.Logger
+import uk.gov.hmrc.clamav.config.ClamAvConfig._
+
+import scalaz.concurrent.Future
+
+object FakeClam {
+
+  def connect() = {
+    val serverSocket = new ServerSocket(0)
+    Logger.warn(s"Fake clam started on port ${serverSocket.getLocalPort}")
+
+    Future {
+      val socket = serverSocket.accept()
+      val out = new DataOutputStream(socket.getOutputStream)
+      val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
+      handle(in, out)
+      socket.close()
+      serverSocket.close()
+    }.start
+    serverSocket
+  }
+
+
+  def handle(in: BufferedReader, out: DataOutputStream): Unit = {
+    val received = new String(Iterator.continually(in.read)
+      .takeWhile(a => a != 0)
+      .map(_.toByte).toArray)
+
+    Logger.warn(s"Received $received")
+    received match {
+      case "zINSTREAM" | "" => handle(in, out)
+      case _ =>
+        Logger.warn(s"Responding with $okClamAvResponse")
+        out.writeBytes(okClamAvResponse)
+        out.flush()
+    }
+  }
+}
