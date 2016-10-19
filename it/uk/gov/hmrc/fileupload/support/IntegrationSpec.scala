250dfd99b8de0cd8d298386cff56e5db2a10c7dc
ralphcollett <ralph.collett@gmail.com>
2016-10-19 11:03:20 +0100

    Use shared Fake Clam for integration test.

--- it/uk/gov/hmrc/fileupload/support/IntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/support/IntegrationSpec.scala
@@ -1,27 +1,45 @@
 package uk.gov.hmrc.fileupload.support
 
+import java.net.ServerSocket
 import java.util.UUID
 
 import org.scalatest.FeatureSpec
 import org.scalatestplus.play.OneServerPerSuite
 import play.api.test.FakeApplication
-import uk.gov.hmrc.fileupload.FakeClam
+import uk.gov.hmrc.clamav.fake.FakeClam
 import uk.gov.hmrc.fileupload.transfer.FakeFileUploadBackend
 
+import scala.concurrent.ExecutionContext
+
 trait IntegrationSpec extends FeatureSpec with OneServerPerSuite with FakeFileUploadBackend {
+
+  implicit val ecIntegrationSpec = ExecutionContext.global
 
   override lazy val port: Int = 9000
 
   val nextId = () => UUID.randomUUID().toString
 
-  lazy private val clamPort = FakeClam.connect().fold(error => fail("Fake clam error", error.cause), _.getLocalPort)
+  lazy val fakeClamSocket = new ServerSocket(0)
+  lazy val fakeClam = new FakeClam(fakeClamSocket)
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+
+    fakeClam.start()
+  }
+
+  override def afterAll(): Unit = {
+    super.afterAll()
+
+    fakeClam.stop()
+  }
 
   implicit override lazy val app: FakeApplication =
     FakeApplication(
       additionalConfiguration = Map(
         "auditing.enabled" -> "false",
         "Test.clam.antivirus.host" -> "127.0.0.1",
-        "Test.clam.antivirus.port" -> clamPort,
+        "Test.clam.antivirus.port" -> fakeClamSocket.getLocalPort,
         "microservice.services.file-upload-backend.port" -> backend.port()
       )
     )
