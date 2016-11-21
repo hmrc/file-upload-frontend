284698c24cc6d6664ecdea5baae94ef206779ca2
Konrad Wudkowski <wudkowski@gmail.com>
2016-11-21 16:01:29 +0000

    Added logging erroneous cases when trying to upload a file.

--- app/uk/gov/hmrc/fileupload/controllers/EnvelopeChecker.scala
+++ app/uk/gov/hmrc/fileupload/controllers/EnvelopeChecker.scala
@@ -17,33 +17,37 @@
 package uk.gov.hmrc.fileupload.controllers
 
 import cats.data.Xor
+import play.api.Logger
+import play.api.http.Status._
 import play.api.libs.iteratee.{Done, Iteratee}
 import play.api.libs.json.Json
-import play.api.mvc.EssentialAction
+import play.api.mvc.{EssentialAction, RequestHeader, Result}
 import play.api.mvc.Results._
 import uk.gov.hmrc.fileupload.EnvelopeId
 import uk.gov.hmrc.fileupload.transfer.TransferService.{EnvelopeStatusNotFoundError, _}
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object EnvelopeChecker {
 
   def withExistingEnvelope(envelopeId: EnvelopeId, check: (EnvelopeId) => Future[EnvelopeStatusResult])
-                          (block: EssentialAction) = EssentialAction { rh =>
+                          (block: EssentialAction)
+                          (implicit ec: ExecutionContext) = EssentialAction { implicit rh =>
     Iteratee.flatten {
       check(envelopeId).map {
         case Xor.Right("OPEN") => block(rh)
         case Xor.Right(otherStatus) =>
-          Done(Locked(msg(s"Unable to upload to envelope: $envelopeId with status: $otherStatus")))
+          logAndReturn(LOCKED, s"Unable to upload to envelope: $envelopeId with status: $otherStatus")
         case Xor.Left(EnvelopeStatusNotFoundError(_)) =>
-          Done(NotFound(msg(s"Unable to upload to nonexistent envelope: $envelopeId")))
+          logAndReturn(NOT_FOUND, s"Unable to upload to nonexistent envelope: $envelopeId")
         case Xor.Left(error) =>
-          Done(InternalServerError(msg(error.toString)))
+          logAndReturn(INTERNAL_SERVER_ERROR, error.toString)
       }
     }
   }
 
-  private def msg(s: String) = Json.obj("message" -> s)
-
+  private def logAndReturn(statusCode: Int, problem: String)(implicit rh: RequestHeader) = {
+    Logger.warn(s"Request: $rh failed because: $problem")
+    Done[Array[Byte], Result](new Status(statusCode).apply{ Json.obj("message" -> problem) })
+  }
 }
