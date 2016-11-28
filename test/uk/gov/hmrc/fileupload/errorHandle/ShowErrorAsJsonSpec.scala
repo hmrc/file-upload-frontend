17d8f0767b4e5ccf51a6aab7e3537598a84e72ce
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2016-11-28 13:40:12 +0000

    FILE-302: Tests are done under units tests

--- test/uk/gov/hmrc/fileupload/errorHandle/ShowErrorAsJsonSpec.scala
+++ test/uk/gov/hmrc/fileupload/errorHandle/ShowErrorAsJsonSpec.scala
@@ -2,14 +2,15 @@ package uk.gov.hmrc.fileupload.errorHandle
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mock.MockitoSugar
-import org.scalatest.{Matchers, WordSpecLike}
 import play.api.GlobalSettings
 import play.api.http.MimeTypes
 import play.api.mvc.RequestHeader
+import play.api.test.Helpers.{contentType, _}
 import uk.gov.hmrc.fileupload.utils.ShowErrorAsJson
 import uk.gov.hmrc.play.http.{BadRequestException, NotFoundException, UnauthorizedException}
+import uk.gov.hmrc.play.test.UnitSpec
 
-class ShowErrorAsJsonSpec extends WordSpecLike with Matchers with ScalaFutures with MockitoSugar {
+class ShowErrorAsJsonSpec extends UnitSpec with ScalaFutures with MockitoSugar {
 
   val jsh = new GlobalSettings with ShowErrorAsJson
 
@@ -18,36 +19,27 @@ class ShowErrorAsJsonSpec extends WordSpecLike with Matchers with ScalaFutures w
   "error handling in onError function" should {
 
     "convert a NotFoundException to NotFound response" in {
-      val ex = new NotFoundException("test")
-      val resultF = jsh.onError(requestHeader, ex).futureValue
-      resultF.header.status shouldBe 404
-
-      val contentType = resultF.header.headers("Content-Type").split(";")
-      contentType(0) shouldBe MimeTypes.JSON
+      val result = jsh.onError(requestHeader, new NotFoundException("test")).futureValue
+      result.header.status shouldBe 404
+      contentType(result).get shouldBe MimeTypes.JSON
     }
 
     "convert a BadRequestException to NotFound response" in {
-      val resultF = jsh.onError(requestHeader, new BadRequestException("bad request")).futureValue
-      resultF.header.status shouldBe 400
-
-      val contentType = resultF.header.headers("Content-Type").split(";")
-      contentType(0) shouldBe MimeTypes.JSON
+      val result = jsh.onError(requestHeader, new BadRequestException("bad request")).futureValue
+      result.header.status shouldBe 400
+      contentType(result).get shouldBe MimeTypes.JSON
     }
 
     "convert an UnauthorizedException to Unauthorized response" in {
-      val resultF = jsh.onError(requestHeader, new UnauthorizedException("unauthorized")).futureValue
-      resultF.header.status shouldBe 401
-
-      val contentType = resultF.header.headers("Content-Type").split(";")
-      contentType(0) shouldBe MimeTypes.JSON
+      val result = jsh.onError(requestHeader, new UnauthorizedException("unauthorized")).futureValue
+      result.header.status shouldBe 401
+      contentType(result).get shouldBe MimeTypes.JSON
     }
 
     "convert an Exception to InternalServerError" in {
-      val resultF = jsh.onError(requestHeader, new Exception("any application exception")).futureValue
-      resultF.header.status shouldBe 500
-
-      val contentType = resultF.header.headers("Content-Type").split(";")
-      contentType(0) shouldBe MimeTypes.JSON
+      val result = jsh.onError(requestHeader, new Exception("any application exception")).futureValue
+      result.header.status shouldBe 500
+      contentType(result).get shouldBe MimeTypes.JSON
     }
 
   }
