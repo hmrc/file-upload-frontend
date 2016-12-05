adfc8b3f3b3af7e5dc38ab4aeba3f28753d49484
Craig Duncan <craig@CraigMBPR.local>
2016-12-05 16:47:18 +0000

    FILE-294 fixed issue with socket conflicts across multiple tests

--- it/uk/gov/hmrc/fileupload/support/IntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/support/IntegrationSpec.scala
@@ -3,7 +3,7 @@ package uk.gov.hmrc.fileupload.support
 import java.net.ServerSocket
 import java.util.UUID
 
-import org.scalatest.FeatureSpec
+import org.scalatest.{BeforeAndAfterEach, FeatureSpec}
 import org.scalatestplus.play.OneServerPerSuite
 import play.api.test.FakeApplication
 import uk.gov.hmrc.clamav.fake.FakeClam
@@ -11,24 +11,25 @@ import uk.gov.hmrc.mongo.MongoSpecSupport
 
 import scala.concurrent.ExecutionContext
 
-trait IntegrationSpec extends FeatureSpec with MongoSpecSupport with OneServerPerSuite with FakeFileUploadBackend {
+trait IntegrationSpec extends FeatureSpec with MongoSpecSupport with OneServerPerSuite with FakeFileUploadBackend with BeforeAndAfterEach {
 
   override lazy val port: Int = 9000
 
   val nextId = () => UUID.randomUUID().toString
 
-  lazy val fakeClamSocket = new ServerSocket(0)
-  lazy val fakeClam = new FakeClam(fakeClamSocket)(ExecutionContext.global)
+  // creating and closing a ServerSocket in order to get a free port
+  val fakeClamSocket = new ServerSocket(0)
+  val socketPort = fakeClamSocket.getLocalPort
+  fakeClamSocket.close()
 
-  override def beforeAll(): Unit = {
-    super.beforeAll()
+  var fakeClam: FakeClam = _
 
+  override def beforeEach(): Unit = {
+    fakeClam = new FakeClam(new ServerSocket(socketPort))(ExecutionContext.global)
     fakeClam.start()
   }
 
-  override def afterAll(): Unit = {
-    super.afterAll()
-
+  override def afterEach(): Unit = {
     fakeClam.stop()
   }
 
@@ -37,7 +38,7 @@ trait IntegrationSpec extends FeatureSpec with MongoSpecSupport with OneServerPe
       additionalConfiguration = Map(
         "auditing.enabled" -> "false",
         "Test.clam.antivirus.host" -> "127.0.0.1",
-        "Test.clam.antivirus.port" -> fakeClamSocket.getLocalPort,
+        "Test.clam.antivirus.port" -> socketPort,
         "microservice.services.file-upload-backend.port" -> backend.port(),
         "mongodb.uri" -> s"mongodb://localhost:27017/$databaseName"
       )
