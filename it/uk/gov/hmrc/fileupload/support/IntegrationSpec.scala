52aea08bc635e0207151548fa3cb95f3e6c07afc
ralphcollett <ralph.collett@gmail.com>
2017-02-01 17:17:49 +0000

    Revert change as broke Jenkins.

--- it/uk/gov/hmrc/fileupload/support/IntegrationSpec.scala
+++ it/uk/gov/hmrc/fileupload/support/IntegrationSpec.scala
@@ -3,7 +3,7 @@ package uk.gov.hmrc.fileupload.support
 import java.net.ServerSocket
 import java.util.UUID
 
-import org.scalatest.FeatureSpec
+import org.scalatest.{BeforeAndAfterEach, FeatureSpec}
 import org.scalatestplus.play.OneServerPerSuite
 import play.api.test.FakeApplication
 import uk.gov.hmrc.clamav.fake.FakeClam
@@ -11,7 +11,7 @@ import uk.gov.hmrc.mongo.MongoSpecSupport
 
 import scala.concurrent.ExecutionContext
 
-trait IntegrationSpec extends FeatureSpec with MongoSpecSupport with OneServerPerSuite with FakeFileUploadBackend {
+trait IntegrationSpec extends FeatureSpec with MongoSpecSupport with OneServerPerSuite with FakeFileUploadBackend with BeforeAndAfterEach {
 
   override lazy val port: Int = 9000
 
@@ -24,16 +24,12 @@ trait IntegrationSpec extends FeatureSpec with MongoSpecSupport with OneServerPe
 
   var fakeClam: FakeClam = _
 
-  override def beforeAll(): Unit = {
-    super.beforeAll()
-
+  override def beforeEach(): Unit = {
     fakeClam = new FakeClam(new ServerSocket(socketPort))(ExecutionContext.global)
     fakeClam.start()
   }
 
-  override def afterAll(): Unit = {
-    super.afterAll()
-
+  override def afterEach(): Unit = {
     fakeClam.stop()
   }
 
