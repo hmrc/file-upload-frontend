e2bf8b4fc8ea35d431db141e35ab66e6dc2ee17b
ralphcollett <ralph.collett@gmail.com>
2017-02-02 09:31:22 +0000

    Revert change as broke Jenkins.

--- it/uk/gov/hmrc/fileupload/support/FakeFileUploadBackend.scala
+++ it/uk/gov/hmrc/fileupload/support/FakeFileUploadBackend.scala
@@ -1,7 +1,5 @@
 package uk.gov.hmrc.fileupload.support
 
-import java.net.ServerSocket
-
 import com.github.tomakehurst.wiremock.WireMockServer
 import com.github.tomakehurst.wiremock.client.WireMock._
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
@@ -9,11 +7,8 @@ import com.github.tomakehurst.wiremock.verification.LoggedRequest
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{BeforeAndAfterAll, Suite}
 import play.api.http.Status
-import uk.gov.hmrc.clamav.fake.FakeClam
 import uk.gov.hmrc.fileupload.{EnvelopeId, FileId}
-
 import scala.collection.JavaConverters._
-import scala.concurrent.ExecutionContext
 
 trait FakeFileUploadBackend extends BeforeAndAfterAll with ScalaFutures {
   this: Suite =>
@@ -22,18 +17,8 @@ trait FakeFileUploadBackend extends BeforeAndAfterAll with ScalaFutures {
 
   final lazy val fileUploadBackendBaseUrl = s"http://localhost:${backend.port()}"
 
-  var fakeClamSocketPort: Int = _
-
-  var fakeClam: FakeClam = _
-
   override def beforeAll() = {
     super.beforeAll()
-    // creating and closing a ServerSocket in order to get a free port
-    val fakeClamSocket = new ServerSocket(0)
-    fakeClamSocketPort = fakeClamSocket.getLocalPort
-    fakeClam = new FakeClam(fakeClamSocket)(ExecutionContext.global)
-    fakeClam.start()
-
     backend.start()
     backend.addStubMapping(
       post(urlPathMatching("/file-upload/events/*"))
@@ -44,8 +29,6 @@ trait FakeFileUploadBackend extends BeforeAndAfterAll with ScalaFutures {
   override def afterAll() = {
     super.afterAll()
     backend.stop()
-
-    fakeClam.stop()
   }
 
   val ENVELOPE_OPEN_RESPONSE = """ { "status" : "OPEN" } """
