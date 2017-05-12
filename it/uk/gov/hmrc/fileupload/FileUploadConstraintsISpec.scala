27add8b51f553694ce944d36e1fe8826b8fdd36b
paul.tan <paul.tan@digital.hmrc.gov.uk>
2017-05-12 14:22:25 +0100

    Corrected FileUploadConstraintsISpec

--- it/uk/gov/hmrc/fileupload/FileUploadConstraintsISpec.scala
+++ it/uk/gov/hmrc/fileupload/FileUploadConstraintsISpec.scala
@@ -1,11 +1,11 @@
 package uk.gov.hmrc.fileupload
 
-import org.scalatest.{FeatureSpecLike, Matchers}
+import org.scalatest.{FeatureSpecLike, GivenWhenThen, Matchers}
 import org.scalatest.concurrent.Eventually
 import uk.gov.hmrc.fileupload.support.{ChunksMongoRepository, EnvelopeActions, FileActions}
 import uk.gov.hmrc.fileupload.DomainFixtures._
 
-class FileUploadConstraintsISpec extends FeatureSpecLike with FileActions with EnvelopeActions with Eventually with Matchers{
+class FileUploadConstraintsISpec extends FeatureSpecLike with FileActions with EnvelopeActions with Eventually with Matchers with GivenWhenThen{
 
   val fileId = anyFileId
   val envelopeId = anyEnvelopeId
@@ -13,30 +13,40 @@ class FileUploadConstraintsISpec extends FeatureSpecLike with FileActions with E
   feature("File Upload Frontend with Constraints") {
 
     scenario("Prevent uploading file that is larger than maxSizePerItem specified in envelope") {
-      Wiremock.respondToEnvelopeCheck(envelopeId, body = ENVELOPE_OPEN_RESPONSE)
 
       val repository = new ChunksMongoRepository(mongo)
       repository.removeAll().futureValue
       def numberOfChunks = repository.findAll().futureValue.size
       numberOfChunks shouldBe 0
 
+      Given("Envelope created with specified maxSizePerItem: 10Mb")
+      Wiremock.respondToEnvelopeCheck(envelopeId, body = ENVELOPE_OPEN_RESPONSE)
+
+      When("Attempting to upload a file larger than 10MB")
       val result = uploadDummyLargeFile(envelopeId, fileId)
+
+      Then("Will Recieve 413 Entity Too Large")
       result.status shouldBe 413
+
+      And("No chunks created because file was not uploaded")
       numberOfChunks shouldBe 0
     }
 
-    scenario("Upload unsupported file type that is not listed in content types specified in envelope") {
-      Wiremock.respondToEnvelopeCheck(envelopeId, body = ENVELOPE_OPEN_RESPONSE)
+    scenario("Upload file of unsupported type that is not listed in content types specified in envelope") {
 
       val repository = new ChunksMongoRepository(mongo)
       repository.removeAll().futureValue
       def numberOfChunks = repository.findAll().futureValue.size
       numberOfChunks shouldBe 0
 
-      val result = uploadDummyInvalidContentTypeFile(envelopeId, fileId)
-      result.underlying
+      Given("Envelope created with specified contentTypes: application/pdf, image/jpeg and application/xml")
+      Wiremock.respondToEnvelopeCheck(envelopeId, body = ENVELOPE_OPEN_RESPONSE)
+
+      When("File uploaded is of an unsupported type")
+      val result = uploadDummyUnsupportedContentTypeFile(envelopeId, fileId)
+
+      Then("Return 200")
       result.status shouldBe 200
-      numberOfChunks shouldBe 0
     }
   }
 
