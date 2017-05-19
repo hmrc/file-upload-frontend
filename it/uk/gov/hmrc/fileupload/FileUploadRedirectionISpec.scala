9fd09f53206301531fc5595b50a646fa90feddb1
Waldemar Wosiński <kastoestoramadus@gmail.com>
2017-05-19 18:22:04 +0100

    refactoring

--- it/uk/gov/hmrc/fileupload/FileUploadRedirectionISpec.scala
+++ it/uk/gov/hmrc/fileupload/FileUploadRedirectionISpec.scala
@@ -8,12 +8,18 @@ import uk.gov.hmrc.fileupload.support.{EnvelopeActions, FileActions}
 
 
 class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with FileActions with EnvelopeActions with Eventually with Matchers {
+  val FAIL_REDIRECT_PARAM_NAME = "redirect-error-url"
+  val SUCC_REDIRECT_PARAM_NAME = "redirect-success-url"
+
+  val DEV_YOUR_PAY = "https://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
+  val QA_YOUR_PAY = "https://www-qa.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
+  val NO_HTTPS = DEV_YOUR_PAY.replaceAll("https", "http")
+  val NOT_GOV_DOMAIN = "https://www.playframework.com/documentation/2.5.x/ScalaTestingWithScalaTest"
 
   feature("Redirect End User when provided with redirect parameters as part of upload file request") {
 
     val fileId = anyFileId
     val envelopeId = anyEnvelopeId
-
     scenario("Redirect upon success to valid url - only success url provided") {
 
       Given("Envelope created with default parameters")
@@ -21,10 +27,10 @@ class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with
       Wiremock.respondToEnvelopeCheck(envelopeId)
 
       When("a file is uploaded provided a redirect on success to a https://www-dev.tax.service.gov.uk url")
-      val redirectSuccessUrl = "https://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-      val queryParam = s"redirect-success-url=$redirectSuccessUrl"
-      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
 
+      val redirectSuccessUrl = DEV_YOUR_PAY
+      val queryParam = s"$SUCC_REDIRECT_PARAM_NAME=$redirectSuccessUrl"
+      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
       Then("upon success the user should be redirected to the url specified in the query parameter")
       uploadFileResponse.status should be(301)
       uploadFileResponse.header("Location").get shouldBe redirectSuccessUrl
@@ -37,32 +43,31 @@ class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with
       Wiremock.respondToEnvelopeCheck(envelopeId)
 
       When("a file is uploaded provided a redirect on success to a https://www-dev.tax.service.gov.uk url")
-      val redirectSuccessUrl = "https://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-      val redirectErrorUrl = "https://www-qa.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-      val queryParam = s"redirect-success-url=$redirectSuccessUrl&redirect-error-url=$redirectErrorUrl"
-      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
+      val redirectSuccessUrl = DEV_YOUR_PAY
+      val redirectErrorUrl = QA_YOUR_PAY
 
+      val queryParam = s"$SUCC_REDIRECT_PARAM_NAME=$redirectSuccessUrl&$FAIL_REDIRECT_PARAM_NAME=$redirectErrorUrl"
+      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
       Then("upon success the user should be redirected to the url specified in the query parameter")
       uploadFileResponse.status should be(301)
       uploadFileResponse.header("Location").get shouldBe redirectSuccessUrl
     }
 
-//    scenario("Redirect upon error to valid url - both success and error urls provided") {
-//
-//      Given("An envelope which does not exist")
-//      val invalidEnvelopeId: EnvelopeId = "123456789"
-//
-//      When("a file is uploaded provided a redirect on error to a https://www-dev.tax.service.gov.uk url")
-//      val redirectSuccessUrl = "https://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-//      val redirectErrorUrl = "https://www-qa.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-//      val queryParam = s"redirect-success-url=$redirectSuccessUrl&redirect-error-url=$redirectErrorUrl"
-//      val uploadFileResponse = uploadDummyFileWithRedirects(invalidEnvelopeId, fileId, queryParam)
-//
-//      Then("upon success the user should be redirected to the url specified in the query parameter")
-//      uploadFileResponse.status should be(301)
-//      uploadFileResponse.header("Location") shouldBe redirectErrorUrl
-//    }
-
+    //    scenario("Redirect upon error to valid url - both success and error urls provided") {
+    //
+    //      Given("An envelope which does not exist")
+    //      val invalidEnvelopeId: EnvelopeId = "123456789"
+    //
+    //      When("a file is uploaded provided a redirect on error to a https://www-dev.tax.service.gov.uk url")
+    //      val redirectSuccessUrl = "https://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
+    //      val redirectErrorUrl = "https://www-qa.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
+    //      val queryParam = s"redirect-success-url=$redirectSuccessUrl&redirect-error-url=$redirectErrorUrl"
+    //      val uploadFileResponse = uploadDummyFileWithRedirects(invalidEnvelopeId, fileId, queryParam)
+    //
+    //      Then("upon success the user should be redirected to the url specified in the query parameter")
+    //      uploadFileResponse.status should be(301)
+    //      uploadFileResponse.header("Location") shouldBe redirectErrorUrl
+    //    }
 
 
     scenario("Redirect upon success to invalid url - not https") {
@@ -71,10 +76,10 @@ class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with
       Wiremock.respondToEnvelopeCheck(envelopeId)
 
       When("a file is uploaded provided a redirect on success to a https://www-dev.tax.service.gov.uk url")
-      val redirectSuccessUrl = "http://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-      val queryParam = s"redirect-success-url=$redirectSuccessUrl"
-      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
+      val redirectSuccessUrl = NO_HTTPS
+      val queryParam = s"$SUCC_REDIRECT_PARAM_NAME=$redirectSuccessUrl"
 
+      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
       Then("a Bad Request response should be received")
       uploadFileResponse.status should be(400)
 
@@ -91,10 +96,10 @@ class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with
       Wiremock.respondToEnvelopeCheck(envelopeId)
 
       When("a file is uploaded provided a redirect on success to a https://www-dev.tax.service.gov.uk url")
-      val redirectErrorUrl = "http://www-dev.tax.service.gov.uk/estimate-paye-take-home-pay/your-pay"
-      val queryParam = s"redirect-error-url=$redirectErrorUrl"
-      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
+      val redirectErrorUrl = NO_HTTPS
+      val queryParam = s"$FAIL_REDIRECT_PARAM_NAME=$redirectErrorUrl"
 
+      val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
       Then("a Bad Request response should be received")
       uploadFileResponse.status should be(400)
 
@@ -112,8 +117,8 @@ class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with
       Wiremock.respondToEnvelopeCheck(envelopeId)
 
       When("a file is uploaded provided a redirect on success to an https but non gov.uk url")
-      val redirectSuccessUrl = "https://www.playframework.com/documentation/2.5.x/ScalaTestingWithScalaTest"
-      val queryParam = s"redirect-success-url=$redirectSuccessUrl"
+      val redirectSuccessUrl = NOT_GOV_DOMAIN
+      val queryParam = s"$SUCC_REDIRECT_PARAM_NAME=$redirectSuccessUrl"
       val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
 
       Then("a Bad Request response should be received")
@@ -132,8 +137,8 @@ class FileUploadRedirectionISpec extends FeatureSpecLike with GivenWhenThen with
       Wiremock.respondToEnvelopeCheck(envelopeId)
 
       When("a file is uploaded provided a redirect on error to an https but non gov.uk url")
-      val redirectErrorUrl = "https://www.playframework.com/documentation/2.5.x/ScalaTestingWithScalaTest"
-      val queryParam = s"redirect-error-url=$redirectErrorUrl"
+      val redirectErrorUrl = NOT_GOV_DOMAIN
+      val queryParam = s"$FAIL_REDIRECT_PARAM_NAME=$redirectErrorUrl"
       val uploadFileResponse = uploadDummyFileWithRedirects(envelopeId, fileId, queryParam)
 
       Then("a Bad Request response should be received")
