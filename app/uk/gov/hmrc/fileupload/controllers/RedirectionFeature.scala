5198223d496714d20bee69474d2590b01b8d6b68
Waldemar Wosiński <kastoestoramadus@gmail.com>
2017-05-19 12:03:00 +0100

    less feedback for the API client

--- app/uk/gov/hmrc/fileupload/controllers/RedirectionFeature.scala
+++ app/uk/gov/hmrc/fileupload/controllers/RedirectionFeature.scala
@@ -19,29 +19,31 @@ package uk.gov.hmrc.fileupload.controllers
 
 import java.net.{MalformedURLException, URL}
 
+import akka.util.ByteString
 import com.typesafe.config.Config
+import play.api.Logger
 import play.api.http.HttpEntity
 import play.api.http.Status.{BAD_REQUEST, MOVED_PERMANENTLY}
+import play.api.libs.iteratee.Done
+import play.api.libs.json.Json
+import play.api.libs.streams.Accumulator
+import play.api.mvc.Results.Status
 import play.api.mvc._
+import uk.gov.hmrc.fileupload.utils.StreamsConverter
 
 import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success, Try}
 
 
 class RedirectionFeature(allowedHosts: Seq[String]) {
+  import RedirectionFeature._
 
   def this(config: Config) =
     this(config.getString("controllers.redirection.allowedHosts")
       .split(",").toSeq
       .map(_.trim))
 
-  val LOCAL_HOST = "localhost"
   private val isLocalHostAllowed = allowedHosts.contains(LOCAL_HOST)
-
-  import EnvelopeChecker.logAndReturn
-
-  case class ValidatedUrl(url: String)
-  case class RedirectUrlsO(succ: Option[ValidatedUrl], fail: Option[ValidatedUrl])
 
   def redirect(successUrlO: Option[String], failureUrlO: Option[String])
               (task: => EssentialAction)
@@ -55,7 +57,7 @@ class RedirectionFeature(allowedHosts: Seq[String]) {
     }
 
     validation match {
-      case Failure(ex) => logAndReturn(BAD_REQUEST, ex.toString)
+      case Failure(ex) => logUrlProblemAndReturn(BAD_REQUEST, ex)
       case Success(redirectParams) =>
         task(rh).map{ wrappedResult =>
           val newUrlO = {
@@ -74,15 +76,6 @@ class RedirectionFeature(allowedHosts: Seq[String]) {
     }
   }
 
-  def redirectToUrl(url: ValidatedUrl): Result = Results.Redirect(url.url, MOVED_PERMANENTLY)
-
-  def addErrorDataToUrl(status: Int, msg: String): ValidatedUrl => ValidatedUrl =
-    baseUrl => ValidatedUrl(baseUrl.url + s"?errorCode:$status&reason=$msg")
-
-  def extractErrorMsg(result: Result): String = {
-    result.body.asInstanceOf[HttpEntity.Strict].data.decodeString("utf-8") // how to do it cleanly?
-  }
-
   def validateAndSanitize(url: String): Try[ValidatedUrl] = {
     Try{
       val suspect = new URL(url)
@@ -91,8 +84,22 @@ class RedirectionFeature(allowedHosts: Seq[String]) {
       if(!allowedHosts.exists(base => suspect.getHost.endsWith(base)))
         throw new MalformedURLException("Given redirection domain is not allowed.")
     }.map( _ => ValidatedUrl(
-    url.takeWhile( c => !(c=='?' || c=='#'))
+      url.takeWhile( c => !(c=='?' || c=='#'))
     ))
+  }
+}
+
+object RedirectionFeature {
+  val LOCAL_HOST = "localhost"
+
+  case class ValidatedUrl(url: String)
+  case class RedirectUrlsO(succ: Option[ValidatedUrl], fail: Option[ValidatedUrl])
+
+  def logUrlProblemAndReturn(statusCode: Int, problem: Throwable)
+                            (implicit rh: RequestHeader): Accumulator[ByteString, Result] = {
+    Logger.warn(s"Request: $rh failed because: ${problem.toString}")
+    val iteratee = Done[Array[Byte], Result](new Status(statusCode).apply(Json.obj("message" -> "URL is invalid")))
+    StreamsConverter.iterateeToAccumulator(iteratee)
   }
   // with cats it should be much better:
   private def optTry2TryOpt(sO: Option[Try[ValidatedUrl]], fO: Option[Try[ValidatedUrl]]) = {
@@ -102,5 +109,15 @@ class RedirectionFeature(allowedHosts: Seq[String]) {
       Failure(failureCheck.head.failed.get)
     else
       Success(RedirectUrlsO(sO.map(_.get), fO.map(_.get)))
+  }
+
+  def redirectToUrl(url: ValidatedUrl): Result =
+    Results.Redirect(url.url, MOVED_PERMANENTLY)
+
+  def addErrorDataToUrl(status: Int, msg: String): ValidatedUrl => ValidatedUrl =
+    baseUrl => ValidatedUrl(baseUrl.url + s"?errorCode:$status&reason=$msg")
+
+  def extractErrorMsg(result: Result): String = {
+    result.body.asInstanceOf[HttpEntity.Strict].data.decodeString("utf-8") // how to do it cleanly?
   }
 }
