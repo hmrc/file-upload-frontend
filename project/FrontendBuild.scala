6a1c9a293481f553af68493d605706df8e830fb1
Joanna Pinto Paul <joanna.pinto@gmail.com>
2018-05-01 10:30:33 +0100

    PLATY-531: Bumped test library dependency.

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -82,7 +82,7 @@ private object AppDependencies {
         "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope,
         "org.pegdown" % "pegdown" % "1.6.0" % scope,
         "org.jsoup" % "jsoup" % "1.8.3" % scope,
-        "uk.gov.hmrc" %% "reactivemongo-test" % "1.6.0" % scope,
+        "uk.gov.hmrc" %% "reactivemongo-test" % "2.0.0" % scope,
         "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
         "com.typesafe.akka" %% "akka-testkit" % "2.3.2" % scope
       )
